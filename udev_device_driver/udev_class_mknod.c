106759ee1071ff99aec4d2199091d6456ef5844a
Toan Dinh <congtoananonymous@gmail.com>
2021-03-23 19:12:23 +0700

    Allocate memory for physical device by kmalloc.
    Signed-off-by: Toan Dinh <congtoananonymous@gmail.com>

--- udev_device_driver/udev_class_mknod.c
+++ udev_device_driver/udev_class_mknod.c
@@ -1,7 +1,9 @@
-#include <linux/module.h>
-#include <linux/fs.h>
-#include <linux/device.h>
-#include <linux/cdev.h>
+#include <linux/module.h>   // declare module_init() and module_exit()
+#include <linux/fs.h>       // define func to register and unregister device number 
+#include <linux/device.h>   // define func to create and ddestroy device file
+#include <linux/cdev.h>     // 
+#include <linux/slab.h>     // allocate memory in kernel space, kmalloc(N, GFP_KERNEL) and kzalloc
+#include "udev_class_mknod.h"
 
 #define DEVICE_NAME "mydev"
 #define CLASS_NAME "dev"
@@ -9,12 +11,44 @@
 #define DRIVER_DESC   "A sample character device driver"
 #define DRIVER_VERSION "0.1"
 
-struct vchar_dev {
+typedef struct vchar_dev {
+    unsigned char *control_regs;
+    unsigned char *status_regs;
+    unsigned char *data_regs;
+} vchar_dev_t;
+
+struct vchar_drv {
     dev_t dev_num;
     struct class *dev_class;
     struct class cdev;
     struct device *dev;
+    vchar_dev_t *vchar_hw;
 } vchar_drv;
+
+/****************** device specific - START *******************/
+
+/* init the device */
+int vchar_hw_init(vchar_dev_t *hw) {
+    char *buf;
+    buf = kzalloc(NUM_DEV_REGS * REG_SIZE, GFP_KERNEL);
+    if (!buf) {
+        return -ENOMEM;
+    }
+
+    hw->control_regs = buf;
+    hw->status_regs = hw->control_regs + NUM_CTRL_REGS;
+    hw->data_regs = hw->status_regs + NUM_STS_REGS;
+
+    /* init data register */
+    hw->control_regs[CONTROL_ACCESS_REG] = 0x03;
+    hw->status_regs[DEVICE_STATUS_REG] = 0x03;
+    return 0;
+}
+
+/* exit the device*/
+int vchar_hw_exit(vchar_dev_t *hw) {
+    kfree(hw->control_regs);    
+}
 
 /* register driver */
 static int __init vchar_driver_init(void) {
@@ -41,7 +75,29 @@ static int __init vchar_driver_init(void) {
         goto failed_register_device_node;
     }
     printk("initialize vchar drive successfully");
+    
+    /* allocate memory for data structure of driver */
+    vchar_drv.vchar_hw = kmalloc(sizeof(vchar_dev_t), GFP_KERNEL);
+    if (!vchar_drv.vchar_hw) {
+        printk("Failed to allocate data structure of driver\n");
+        ret = -ENOMEM;
+        goto failed_allocate_structure;
+    }
+    
+    /* init the physical device */
+    ret = vchar_hw_init(vchar_drv.vchar_hw);
+    if (ret < 0) {
+        printk("Failed to initialize a virtual character device\n");
+        goto failed_init_hw;
+    }
+
     return 0;
+
+
+failed_init_hw:
+    kfree(vchar_drv.vchar_hw);   
+failed_allocate_structure:
+    device_destroy(vchar_drv.dev_class, vchar_drv.dev_num);
 failed_register_device_node:
     class_destroy(vchar_drv.dev_class);
 failed_register_class_device:
@@ -51,6 +107,10 @@ failed_register_devnum:
 }
 
 static void __exit vchar_driver_exit(void) {
+    /* exit physical device */
+    vchar_hw_exit(vchar_drv.vchar_hw);
+    /* free kernel buffer*/
+    kfree(vchar_drv.vchar_hw);
     /* unregister device node */
     device_destroy(vchar_drv.dev_class, vchar_drv.dev_num);
     /* unregister device class */
--- udev_device_driver/udev_class_mknod.mod.c
+++ udev_device_driver/udev_class_mknod.mod.c
@@ -18,14 +18,18 @@ static const struct modversion_info ____versions[]
 __used
 __attribute__((section("__versions"))) = {
 	{ 0xd29ace7d, __VMLINUX_SYMBOL_STR(module_layout) },
-	{ 0xe0ddd871, __VMLINUX_SYMBOL_STR(device_destroy) },
-	{ 0x2e5810c6, __VMLINUX_SYMBOL_STR(__aeabi_unwind_cpp_pr1) },
 	{ 0x7485e15e, __VMLINUX_SYMBOL_STR(unregister_chrdev_region) },
 	{ 0x9adaf3a8, __VMLINUX_SYMBOL_STR(class_destroy) },
+	{ 0xe0ddd871, __VMLINUX_SYMBOL_STR(device_destroy) },
 	{ 0xd9e9a524, __VMLINUX_SYMBOL_STR(device_create) },
 	{ 0x7b0d0d08, __VMLINUX_SYMBOL_STR(__class_create) },
 	{ 0x27e1a049, __VMLINUX_SYMBOL_STR(printk) },
 	{ 0x29537c9e, __VMLINUX_SYMBOL_STR(alloc_chrdev_region) },
+	{ 0x37a0cba, __VMLINUX_SYMBOL_STR(kfree) },
+	{ 0x2e5810c6, __VMLINUX_SYMBOL_STR(__aeabi_unwind_cpp_pr1) },
+	{ 0xf6f5580, __VMLINUX_SYMBOL_STR(kmem_cache_alloc_trace) },
+	{ 0xfd6d346, __VMLINUX_SYMBOL_STR(kmalloc_caches) },
+	{ 0xb1ad28e0, __VMLINUX_SYMBOL_STR(__gnu_mcount_nc) },
 };
 
 static const char __module_depends[]
@@ -34,4 +38,4 @@ __attribute__((section(".modinfo"))) =
 "depends=";
 
 
-MODULE_INFO(srcversion, "8A2611F3B3473FC2755E8AC");
+MODULE_INFO(srcversion, "AFBC41BFD696A18560C6CA3");
