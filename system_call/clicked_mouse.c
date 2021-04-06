0f69eb1e13a69550cde4b921b1fe02551bb9c5c1
Dinh Cong Toan <toan.dinhcong@vn.bosch.com>
2021-04-06 16:52:41 +0700

    update clicked_mouse
    Signed-off-by: Dinh Cong Toan <toan.dinhcong@vn.bosch.com>

--- system_call/clicked_mouse.c
+++ system_call/clicked_mouse.c
@@ -6,8 +6,10 @@
 #include <linux/input.h>
 #include <time.h>
 
-#define EVENT4 "/dev/input/event4"
+#define EVENT4 "/dev/input/event5"
 #define EVENT6 "/dev/input/event6"
+
+enum data { LEFT = 272, RIGHT = 273, CENTER = 274, POSITION_x = 0, POSITION_y = 1};
 
 int main(int argc, char *argv[]) {
     int fd;
@@ -26,22 +28,46 @@ int main(int argc, char *argv[]) {
                 fd = open(EVENT6, O_RDONLY);
                 break;
             }
+            case 'h': {
+                printf("Please use option -c or -p");          
+                break;
+            }
             default:
+                printf("Please use option -c or -p");          
                 break;
         };
-    /* 
+     
     if ( flag == 1)
         fd = open(EVENT6, O_RDONLY);
     else 
         fd = open(EVENT4, O_RDONLY);
-    */
-
+    
     while (1) {
         int ret = read(fd, &a, sizeof(a));
         unsigned short code = a.code;
         unsigned short type = a.type;
         unsigned int value = a.value;
-        printf("Code: %d , Type: %d, Value: %d\n", code, type, value);
+        switch(code) {
+            case LEFT: { 
+                if(value == 1)
+                    printf("LEFT mouse is clicked\n");
+                break;
+            }
+            case RIGHT: { 
+                if(value == 1)
+                    printf("RIGHT mouse is clicked\n");
+                break;
+            }
+            case CENTER: { 
+                if(value = 1)
+                    printf("CENTER is clicked\n");
+                break;
+            }
+            default:
+              break;
+        };
+
+      // printf("Code: %d , Type: %d, Value: %d\n", code, type, value);
     }
     return 0;
 }
